c5f646a83233e63a3542e8ef03af18816de38ba1
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-07-31 19:40:53 +0300

    add task model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+    describe "Validations" do
+      it "is valid valid with valid attributes" do
+        task = Task.new(task: "New task", completed: false)
+
+        expect(task).to be_valid
+      end
+      it "is invalid with no task" do
+        task = Task.new(task: "", completed: false)
+        expect(task).not_to be_valid
+        expect(task.errors.to_a).to include("Task can't be blank")
+      end
+      it "is invalid with completed status" do
+        task = Task.new(task: "I am a new task")
+        expect(task).not_to be_valid
+      end
+    end
+  end
