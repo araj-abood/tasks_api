634f8859790563fff360d5eb6a01623f64491ccf
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-07-31 17:39:03 +0300

    add delete endpoint

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,14 @@ class TasksController < ApplicationController
       render json: { success: true, data: task }, status: :created
     end
   end
+  def destroy
+    task = Task.find(params[:id])
+    if task.destroy!
+        head :no_content
+    end
+  end
 
+  private
   def create_params
     params.expect(task: [ :task, :completed ])
   end
