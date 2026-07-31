f2b7779ac1edfeb9bb3907df372628008fee1e2f
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-07-31 17:41:23 +0300

    add show task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [ :destroy, :show ]
+
   def index
     @tasks = Task.all
   end
@@ -9,14 +11,24 @@ class TasksController < ApplicationController
       render json: { success: true, data: task }, status: :created
     end
   end
+
   def destroy
-    task = Task.find(params[:id])
+    @task = Task.find(params[:id])
     if task.destroy!
         head :no_content
     end
   end
 
+  def show
+  end
+
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+
   def create_params
     params.expect(task: [ :task, :completed ])
   end
