6ec6ca32bf36daf51c911a9143d923936b5e97e8
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-07-31 16:47:40 +0300

    add custom tranlations for inclusion

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :task, presence: true
-  validates :completed, inclusion: { in: [ true, false ] }
+  validates :completed, inclusion: { in: [ true, false ], message: "Should be either true / false" }
 end
