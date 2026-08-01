468e9fdb5643a0b8dbde30566750c855220e3eb0
Abdullah al-araj <alaraj.abdullah02@gmail.com>
2026-08-01 23:45:04 +0300

    add generic post

--- app/controllers/concerns/crud_actions.rb
+++ app/controllers/concerns/crud_actions.rb
@@ -3,10 +3,28 @@ module CrudActions
 
 
   def index
-    actual_modal = controller_name.classify.constantize
+    actual_modal = get_model_constant
 
     all_record = actual_modal.all
 
     instance_variable_set("@#{controller_name}", all_record)
+  end
+
+
+
+  def create
+    obj_record = get_model_constant.new(model_params)
+    debugger
+  end
+
+
+
+  private
+  def get_model_constant
+    controller_name.classify.constantize
+  end
+
+  def model_params
+    send("#{controller_name}_param")
   end
 end
